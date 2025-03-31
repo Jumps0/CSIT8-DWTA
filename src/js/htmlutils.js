b8ff5596104c3dbde96ce7c8a3290cbec3ae09b6
Jumps0 <49619518+Jumps0@users.noreply.github.com>
2025-03-31 22:12:47 +0200

    Add (placeholder) additional info text
    Hopefully shouldn't be that hard to modify in the future

--- src/js/htmlutils.js
+++ src/js/htmlutils.js
@@ -216,6 +216,7 @@ let htmlUtils = {
   <a href="" class="removeOrigin">&#10006</a>
   ${htmlUtils.getToggleHtml(fqdn, action, blockedFpScripts)}
   <a href="" class="honeybadgerPowered tooltip" title="${undo_arrow_tooltip}"></a>
+  <p style="color:rgb(238, 255, 0);"><b>This is some text which provides additional information.</b></p>
 </div>
       `.trim();
     };
